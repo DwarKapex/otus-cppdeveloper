85864a3dff0b80f8e88ffef85fe4e27ea802bafa
Vladislav <vlad.kv.2002@gmail.com>
2020-12-24 22:37:43 -0800

    test for Travis: it cannot generate allocator deb

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 cmake .
 # build lib
-cmake --build . --target lib_Allocator
+cmake --build . --target lib_IpFilter
 # build test for lib
-cmake --build . --target test_Allocator
+cmake --build . --target test_IpFilter
 # run test
-ctest test_Allocator
+ctest test_IpFilter
 # build and install app
-cmake --build . --target allocator
+cmake --build . --target ip_filter
 # generate deb package 
 cpack
