674f73a7c06c76767c870ae117d01f6f50361ebb
Vladislav <vlad.kv.2002@gmail.com>
2020-12-28 14:10:38 -0800

    Finished structure development but app

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 cmake .
 # build lib
-cmake --build . --target lib_struct
+cmake --build . --target lib_PrintIp
 # build test for lib
-cmake --build . --target test_struct
+cmake --build . --target test_PrintIp
 # run test
-ctest test_struct
+ctest test_PrintIp
 # build and install app
-cmake --build . --target struct
+cmake --build . --target print_ip
 # generate deb package 
 cpack
