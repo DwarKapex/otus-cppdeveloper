fedd04e7e27c9ee72a1af7461bbc71fb7043c9b3
Vladislav <vlad.kv.2002@gmail.com>
2020-12-29 13:28:05 -0800

    Added empty project as a start point

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 cmake .
 # build lib
-cmake --build . --target lib_struct
+cmake --build . --target lib_Matrix
 # build test for lib
-cmake --build . --target test_struct
+cmake --build . --target test_Matrix
 # run test
-ctest test_struct
+ctest test_Matrix
 # build and install app
-cmake --build . --target struct
+cmake --build . --target matrix
 # generate deb package 
 cpack
