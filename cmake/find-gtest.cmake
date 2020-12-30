da06870185464c656b874bc3b233c40a79e9632c
Vladislav <vlad.kv.2002@gmail.com>
2020-12-30 11:29:42 -0800

    Travis CI to find gtest

--- cmake/find-gtest.cmake
+++ cmake/find-gtest.cmake
@@ -4,7 +4,7 @@ set(GOOGLETEST_ROOT ${CMAKE_CURRENT_SOURCE_DIR}/googletest CACHE STRING "Google 
 if (EXISTS ${GOOGLETEST_ROOT})
     set(GTEST_FOUND TRUE)
     add_subdirectory(
-        ${GOOGLETEST_ROOT}
+        "${GOOGLETEST_ROOT}"
         "googletest"
     )
     include_directories(${gtest_SOURCE_DIR}/include ${gtest_SOURCE_DIR})
